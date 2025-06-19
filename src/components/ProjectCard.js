10f835c1ddaf9a6b6f71c672e3494797dd092542
CJ <cjampion8@gmail.com>
2025-06-19 19:09:43 -0400

    Rearranged project list, added preview images, and added a github link to each project.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,17 @@
+import githubLogo from "../images/github_logo.png";
+
 function ProjectCard({ project }) {
-    return (        <div className="project-card">
+    return (
+        <div className="project-card">
             <a href={project.link} target="_blank" rel="noreferrer" className="project-image-link">
                 <img src={project.image} alt={project.title} className="project-image" />
             </a>
             <div className="project-info">
-                    <a href={project.link} target="_blank" rel="noreferrer" className="project-title">{project.title}</a>
+                    <div className="title-container">
+                        <a href={project.link} target="_blank" rel="noreferrer" className="project-title">{project.title}</a>                        {project.link && <a href={project.github} target="_blank" rel="noreferrer">
+                            <img src={githubLogo} alt="GitHub" className="github-icon" />
+                        </a>}
+                    </div>
                     <p className="project-tools">
                         {project.tools.map((tool, i) => (
                         <span key={i}>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,15 @@
 const projects = [
     {
-        title: "To-Do List",
+        title: "Weather Dashboard",
         tools: [
-            "JavaScript",
-            "CSS"
-        ],
-        image: require("../images/temp.png"),
-        description: "A to-do list application that allows users to add, edit, mark as complete and delete tasks. Users can also add their name to be personally greeted upon opening the app.",
-        link: "https://github.com/CJampion-8/to-do_list"
-        
+            "React",
+            "CSS", 
+            <a href="https://openweathermap.org/" target="_blank" rel="noreferrer">OpenWeather API</a>
+            ],
+        image: require("../images/weather_dashboard_preview.png"),
+        description: "A modern, responsive weather application that provides real-time weather information with dynamic visual effects and animations.",
+        link: "https://cjampion-8.github.io/weather_dashboard/",
+        github: "https://github.com/CJampion-8/weather_dashboard"
     },
     {
         title: "BMI Calculator",
@@ -17,20 +18,22 @@ const projects = [
             "CSS",
             <a href="https://rapidapi.com/principalapis/api/body-mass-index-bmi-calculator" target="_blank" rel="noreferrer">Rapid API</a>
         ],
-        image: require("../images/temp.png"),
-        description: "A BMI calculator that allows users to input their height and weight to calculate their Body Mass Index.",
-        link: ""
+        image: require("../images/bmi_calculator_preview.png"),
+        description: "A responsive React application that calculates Body Mass Index using user input for height and weight. The application integrates with an external API for accurate calculations and provides a backup calculation method for reliability.",
+        link: "https://cjampion-8.github.io/bmi_calculator/",
+        github: "https://github.com/CJampion-8/bmi_calculator"
     },
     {
-        title: "Weather Dashboard",
+        title: "To-Do List",
         tools: [
-            "React",
-            "CSS", 
-            <a href="https://openweathermap.org/" target="_blank" rel="noreferrer">OpenWeather API</a>
-            ],
-        image: require("../images/temp.png"),
-        description: "A weather dashboard that provides current weather information and forecasts for any city.",
-        link: ""
+            "JavaScript",
+            "CSS"
+        ],
+        image: require("../images/to-do_list_preview.png"),
+        description: "A to-do list application that allows users to add, edit, mark as complete and delete tasks. Users can also add their name to be personally greeted upon opening the app.",
+        link: "https://github.com/CJampion-8/to-do_list",
+        github: "https://github.com/CJampion-8/to-do_list"
+        
     },
 
 ];
